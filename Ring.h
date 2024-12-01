e28ccbe46821be7005602ed3b2f0f34a725b3624
Kout272 <130234405+Kout272@users.noreply.github.com>
2024-12-01 18:19:26 +0300

    123

--- Ring.h
+++ Ring.h
@@ -1,6 +1,34 @@
 #pragma once
-class Ring
-{
+#include <iostream>
+using namespace std;
 
+template <typename T>
+class Node
+{
+public:
+    T data;
+    Node<T>* prev;
+    Node<T>* next;
 };
 
+template <typename T>
+class Ring
+{
+    Node<T>* begin;
+    Node<T>* current;
+public:
+    Ring();
+    Ring(const Ring& other);
+    void Push(T data);
+    void Display();
+    void RemoveCurrent();
+    bool Empty();
+    void FindMin();
+    void FindMax();
+    void ShowCurrent();
+    bool Search();
+    void RemoveAll();
+    ~Ring();
+};
+
+#include "Ring.cpp"
